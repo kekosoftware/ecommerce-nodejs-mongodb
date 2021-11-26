c3b222b6b4332709abaef21debc1233ad2f58729
Gustavo Ariel Coronel <gcoronel@vippinn.com>
2021-11-26 08:49:45 -0300

    Add models and routers of products

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app =  express()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+require('dotenv/config');
+const api = process.env.API_URL;
+
+const productsRouter = require('./routers/products')
+
+// Middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+
+// Routers
+app.use(`${api}/products`, productsRouter)
+
+
+mongoose.connect(process.env.CONNECTION_STRING)
+.then(() => {
+  console.log('Database connection is ready... ')
+})
+.catch((err) => {
+  console.log(err)
+})
+
+app.listen(3000, () =>  {
+  console.log("Server is running http://localhost:3000")
+})
