584c02487361245525b677cf92f65c7c78768c35
Gustavo Ariel Coronel <gcoronel@vippinn.com>
2021-11-29 08:01:09 -0300

    Add several list of products

--- routers/products.js
+++ routers/products.js
@@ -2,14 +2,41 @@ const { Product } = require('../models/product');
 const { Category } = require('../models/category');
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
 
 // http://localhost:3000/api/v1/products
-router.get(`/`, async (req, res) => {
-  const productList = await Product.find()
-  if(!productList) {
-    res.status(500).json({success: false})
+router.get(`/`, async (req, res) =>{
+  let filter = {};
+  if(req.query.categories)
+  {
+       filter = {category: req.query.categories.split(',')}
   }
-  res.send(productList)
+
+  const productList = await Product.find(filter).populate('category');
+
+  if(!productList) {
+      res.status(500).json({success: false})
+  } 
+  res.send(productList);
+})
+
+router.get(`/:id`, async (req, res) =>{
+  const product = await Product.findById(req.params.id).populate('category');
+
+  if(!product) {
+      res.status(500).json({success: false})
+  } 
+  res.send(product);
+})
+
+router.get(`/get/featured/:count`, async (req, res) =>{
+  const count = req.params.count ? req.params.count : 0
+  const products = await Product.find({isFeatured: true}).limit(+count);
+
+  if(!products) {
+      res.status(500).json({success: false})
+  } 
+  res.send(products);
 })
 
 router.post(`/`, async (req, res) =>{
@@ -42,5 +69,62 @@ router.post(`/`, async (req, res) =>{
 
   res.send(product);
 })
+
+
+router.put('/:id',async (req, res)=> {
+  if(!mongoose.isValidObjectId(req.params.id)) {
+     return res.status(400).send('Invalid Product Id') 
+  }
+  const category = await Category.findById(req.body.category);
+  if(!category) return res.status(400).send('Invalid Category')
+
+  const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+          name: req.body.name,
+          description: req.body.description,
+          richDescription: req.body.richDescription,
+          image: req.body.image,
+          brand: req.body.brand,
+          price: req.body.price,
+          category: req.body.category,
+          countInStock: req.body.countInStock,
+          rating: req.body.rating,
+          numReviews: req.body.numReviews,
+          isFeatured: req.body.isFeatured,
+      },
+      { new: true}
+  )
+
+  if(!product)
+  return res.status(500).send('the product cannot be updated!')
+
+  res.send(product);
+})
+
+router.delete('/:id', (req, res)=>{
+  Product.findByIdAndRemove(req.params.id).then(product =>{
+      if(product) {
+          return res.status(200).json({success: true, message: 'the product is deleted!'})
+      } else {
+          return res.status(404).json({success: false , message: "product not found!"})
+      }
+  }).catch(err=>{
+     return res.status(500).json({success: false, error: err}) 
+  })
+})
+
+router.get(`/get/count`, async (req, res) =>{
+  const productCount = await Product.countDocuments();
+
+  if(!productCount) {
+      res.status(500).json({success: false})
+  } 
+  res.send({
+      productCount: productCount
+  });
+})
+
+
 
 module.exports = router;
