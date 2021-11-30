9933c985443b9b91bb3b61dab87b1bcc11de854c
Gustavo Ariel Coronel <gcoronel@vippinn.com>
2021-11-30 09:06:45 -0300

    Ended user and authentication section

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app =  express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const cors = require('cors')
-
 require('dotenv/config');
+const authJwt = require('./helpers/jwt')
+const errorHandler = require('./helpers/error-handler');
+
 
 app.use(cors())
 app.options('*', cors())
@@ -12,15 +14,19 @@ app.options('*', cors())
 // Middleware
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(authJwt());
+app.use(errorHandler);
 
 // Routers
 const productsRoutes = require('./routers/products')
 const categoriesRoutes = require('./routers/categories')
+const usersRoutes = require('./routers/users')
 
 const api = process.env.API_URL;
 
 app.use(`${api}/products`, productsRoutes)
 app.use(`${api}/categories`, categoriesRoutes)
+app.use(`${api}/users`, usersRoutes)
 
 
 mongoose.connect(process.env.CONNECTION_STRING)
