85f10c4e57fda90970a13e60008a4cda6111f5cb
Gustavo Ariel Coronel <gcoronel@vippinn.com>
2021-12-08 11:08:43 -0300

    Finished the section of generating orders

--- app.js
+++ app.js
@@ -21,12 +21,14 @@ app.use(errorHandler);
 const productsRoutes = require('./routers/products')
 const categoriesRoutes = require('./routers/categories')
 const usersRoutes = require('./routers/users')
+const ordersRoutes = require('./routers/orders')
 
 const api = process.env.API_URL;
 
 app.use(`${api}/products`, productsRoutes)
 app.use(`${api}/categories`, categoriesRoutes)
 app.use(`${api}/users`, usersRoutes)
+app.use(`${api}/orders`, ordersRoutes)
 
 
 mongoose.connect(process.env.CONNECTION_STRING)
